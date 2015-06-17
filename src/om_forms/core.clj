80b1eb778022213f09785ccc6fe8136117f7efdf
Martin Bilski <martinb@criticue.com>
2015-06-17 17:04:00 +0200

    Add support for nested options to `with-options`.

--- src/om_forms/core.clj
+++ src/om_forms/core.clj
@@ -2,5 +2,12 @@
 
 (defmacro with-options
   [options & body]
-  `(binding [om-forms.core/*options* (cljs.core/merge om-forms.core/*options* ~options)]
+  `(binding [om-forms.core/*options*
+             (cljs.core/merge-with
+               (cljs.core/fn [old# new#]
+                 (cljs.core/cond
+                   (cljs.core/map? old#) (cljs.core/merge old# new#)
+                   :else new#))
+               om-forms.core/*options*
+               ~options)]
      ~@body))
