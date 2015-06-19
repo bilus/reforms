da14e02c64f59b45f9fa1f3d309153da9641854f
Martin Bilski <martinb@criticue.com>
2015-06-19 14:06:50 +0200

    Add styling example to ‘simple’.

--- examples/simple/src/simple.cljs
+++ examples/simple/src/simple.cljs
@@ -49,22 +49,19 @@
     om/IRender
     (render [_]
       (html
-        (f/with-options
-          {:group-title {:tag :h3}
-           :form {:horizontal true}}
-          (f/form
-            (f/group-title {:class "group-title-main"} "Customer info")
-            (f/select "Customer type" customer [:type]
-                      [[:private "Private"] [:corporate "Corporate"]])
-            (f/group-title {:class "group-title-minor"} "Details")
-            (render-details customer)
-            (f/group-title {:class "group-title-minor"} "Address")
-            (render-address customer)
-            (f/group-title {:class "group-title-main"} "Access credentials")
-            (f/text "Login" "Enter login" customer [:login])
-            (f/password "Password" "Enter password" customer [:password])
-            (f/form-buttons
-              (f/button "Save" #(js/alert "clicked")))))))))
+        (f/form
+          (f/group-title {:class "group-title-main"} "Customer info")
+          (f/select "Customer type" customer [:type]
+                    [[:private "Private"] [:corporate "Corporate"]])
+          (f/group-title {:class "group-title-minor"} "Details")
+          (render-details customer)
+          (f/group-title {:class "group-title-minor"} "Address")
+          (render-address customer)
+          (f/group-title {:class "group-title-main"} "Access credentials")
+          (f/text "Login" "Enter login" customer [:login])
+          (f/password "Password" "Enter password" customer [:password])
+          (f/form-buttons
+            (f/button "Save" #(js/alert "clicked"))))))))
 
 (defn main-view
   [app-state _owner]
@@ -77,6 +74,16 @@
          [:br]
          (om/build customer-form-view (:customer app-state))
          (om/build inspector-view app-state)]))))
+
+;; Styling the form using global configuration options. Probably not such a good idea but good for the demo.
+(f/set-options!
+  {:form        {:horizontal true
+                 :attrs      {:style {:border            "1px solid #BBB"
+                                      :border-radius     "5px"
+                                      :background-color "#EFEFEF"
+                                      :padding           "10px 20px"}}}
+   :group-title {:tag :h3
+                 :attrs {:style {:color "#666"}}}})
 
 (om/root
   main-view
