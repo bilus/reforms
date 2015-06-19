5e4d0b746ff9765fd5110428e823cc05b683a7bb
Martin Bilski <martinb@criticue.com>
2015-06-20 00:03:09 +0200

    Remove dependency on sablono from reagent example.

--- examples/reagent/src/reagent.cljs
+++ examples/reagent/src/reagent.cljs
@@ -1,22 +1,21 @@
 (ns examples.reagent
   (:require [reforms.core :as f :include-macros true]
             [reforms.binding.reagent]
-            [reagent.core :as reagent :refer [atom]]
-            [sablono.core :refer-macros [html]]))
+            [reagent.core :refer [atom render-component]]))
 
 (def app-state (atom {:name "John"}))
 
 (defn simple-view
   [data]
-  (html
-    (f/with-options {:form {:horizontal (:orientation-horizontal data)}}
-                    (f/panel
-                      "Hello, world"
-                      (f/form
-                        (f/text "Your name" "Type your name here" data [:name])
-                        (f/form-buttons
-                          (f/button-primary "Submit" #(js/alert (:name @data)))
-                          (f/button-default "Cancel" #(js/alert "Cancel!"))))))))
+  (f/with-options
+    {:form {:horizontal (:orientation-horizontal data)}}
+    (f/panel
+      "Hello, world"
+      (f/form
+        (f/text "Your name" "Type your name here" data [:name])
+        (f/form-buttons
+          (f/button-primary "Submit" #(js/alert (:name @data)))
+          (f/button-default "Cancel" #(js/alert "Cancel!")))))))
 
 (defn inspector-view
   [app-state]
@@ -28,7 +27,7 @@
    [simple-view app-state]
    [inspector-view app-state]])
 
-(reagent/render-component
+(render-component
   [main-view]
   (. js/document (getElementById "app")))
 
