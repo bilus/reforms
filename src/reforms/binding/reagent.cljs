86d105854f4c28bbf344837a6108aa2f79ae763c
Martin Bilski <martinb@criticue.com>
2015-06-20 02:08:17 +0200

    Re-enable Reagent bindings.

--- src/reforms/binding/reagent.cljs
+++ src/reforms/binding/reagent.cljs
@@ -5,39 +5,39 @@
 (when js/console
   (js/console.log "Loading Reagent bindings."))
 
-;(defn- do-reset!
-;  [x ks v]
-;  (swap! x (fn [x]
-;             (assoc-in x ks v))))
-;
-;(extend-type RCursor
-;  IBinding
-;  (-valid? [_]
-;    true)
-;  (-deref [this]
-;    @this)
-;  (-reset!
-;    ([this v]
-;     (reset! this v))
-;    ([this ks v]
-;     (do-reset! this ks v)))
-;  (-get-in [this ks]
-;    (get-in @this ks))
-;  (-path [this]
-;    (.-path this)))
-;
-;(extend-type default
-;  IBinding
-;  (-valid? [this]
-;    (satisfies? IAtom this))
-;  (-deref [this]
-;    @this)
-;  (-reset!
-;    ([this v]
-;     (reset! this v))
-;    ([this ks v]
-;     (do-reset! this ks v)))
-;  (-get-in [this ks]
-;    (get-in @this ks))
-;  (-path [_]
-;    nil))
+(defn- do-reset!
+  [x ks v]
+  (swap! x (fn [x]
+             (assoc-in x ks v))))
+
+(extend-type RCursor
+  IBinding
+  (-valid? [_]
+    true)
+  (-deref [this]
+    @this)
+  (-reset!
+    ([this v]
+     (reset! this v))
+    ([this ks v]
+     (do-reset! this ks v)))
+  (-get-in [this ks]
+    (get-in @this ks))
+  (-path [this]
+    (.-path this)))
+
+(extend-type default
+  IBinding
+  (-valid? [this]
+    (satisfies? IAtom this))
+  (-deref [this]
+    @this)
+  (-reset!
+    ([this v]
+     (reset! this v))
+    ([this ks v]
+     (do-reset! this ks v)))
+  (-get-in [this ks]
+    (get-in @this ks))
+  (-path [_]
+    nil))
