d6bf2bcbaaa61de578523f541ca7a5f8c6172894
Martin Bilski <martinb@criticue.com>
2015-06-22 17:39:52 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,8 @@ then
   cp -r doc/ /tmp/reforms/doc/ &&
   echo "Checkout and pull gh-pages" &&
   git checkout gh-pages &&
-  git pull origin gh-pages &&
+  git pull origin gh-pages
+
   cp -r /tmp/reforms/doc doc/ &&
   echo "Copy examples" &&
   mkdir -p examples/om &&
@@ -42,7 +43,8 @@ then
   git add doc/ examples/ &&
   git commit -m "Update Github Pages." &&
   git push origin gh-pages &&
-  echo "Switch back to $BRANCH branch" &&
+  echo "Switch back to $BRANCH branch"
+  
   git checkout $BRANCH
 fi
 
