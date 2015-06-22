401898002f706aa31f1d2f2ff828e176d50b185d
Martin Bilski <martinb@criticue.com>
2015-06-22 15:54:47 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,25 @@ read -p "Are version numbers in sync? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # # Build Reforms core.
- #  git checkout master &&
- #  doctoc README.md && # npm install -g doctoc
- #  git add README.md &&
- #  git commit -m "Regenerate TOC in Readme." &&
- #  git push origin master &&
- #  lein deploy clojars &&
- #
- #  # Build om-reforms examples.
- #  cd ../om-reforms &&
- #  git checkout master &&
- #  lein deps && lein clean && lein cljsbuild once &&
- #  lein deploy clojars &&
- #
- #  # Build reagent-reforms examples.
- #  cd ../reagent-reforms &&
- #  git checkout master &&
- #  lein deps && lein clean && lein cljsbuild once &&
- #  lein deploy clojars &&
+  # Build Reforms core.
+  git checkout master &&
+  doctoc README.md && # npm install -g doctoc
+  git add README.md &&
+  git commit -m "Regenerate TOC in Readme." &&
+  git push origin master &&
+  lein deploy clojars &&
+
+  # Build om-reforms examples.
+  cd ../om-reforms &&
+  git checkout master &&
+  lein deps && lein clean && lein cljsbuild once &&
+  lein deploy clojars &&
+
+  # Build reagent-reforms examples.
+  cd ../reagent-reforms &&
+  git checkout master &&
+  lein deps && lein clean && lein cljsbuild once &&
+  lein deploy clojars &&
 
   # Build Home page and the reference + examples.
   cd ../reforms &&
