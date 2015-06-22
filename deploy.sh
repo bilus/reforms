02b968071c3bc208a3839a4dcbeeb0abf3a5436a
Martin Bilski <martinb@criticue.com>
2015-06-22 15:39:33 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,25 @@ read -p "Are version numbers in sync? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # Build Reforms core.
-  git checkout master &&
-  doctoc README.md && # npm install -g doctoc
-  git add README.md &&
-  git commit -m "Regenerate TOC in Readme." &&
-  git push origin master &&
-  lein deploy clojars &&
-  git push origin master &&
-
-  # Build om-reforms examples.
-  cd ../om-reforms &&
-  git checkout master &&
-  lein deps && lein clean && lein cljsbuild once &&
-  lein deploy clojars &&
-
-  # Build reagent-reforms examples.
-  cd ../reagent-reforms &&
-  git checkout master &&
-  lein deps && lein clean && lein cljsbuild once &&
-  lein deploy clojars &&
+  # # Build Reforms core.
+ #  git checkout master &&
+ #  doctoc README.md && # npm install -g doctoc
+ #  git add README.md &&
+ #  git commit -m "Regenerate TOC in Readme." &&
+ #  git push origin master &&
+ #  lein deploy clojars &&
+ #
+ #  # Build om-reforms examples.
+ #  cd ../om-reforms &&
+ #  git checkout master &&
+ #  lein deps && lein clean && lein cljsbuild once &&
+ #  lein deploy clojars &&
+ #
+ #  # Build reagent-reforms examples.
+ #  cd ../reagent-reforms &&
+ #  git checkout master &&
+ #  lein deps && lein clean && lein cljsbuild once &&
+ #  lein deploy clojars &&
 
   # Build Home page and the reference + examples.
   cd ../reforms &&
@@ -29,9 +28,9 @@ then
   lein doc &&
   git checkout gh-pages &&
   git pull origin gh-pages &&
-  mkdir examples/om &&
+  mkdir -p examples/om &&
   cp -r ../om-reforms/examples/ examples/om &&
-  mkdir examples/reagent &&
+  mkdir -p examples/reagent &&
   cp -r ../reagent-reforms/examples/ examples/reagent &&
   git add doc/ &&
   git add examples/ &&
