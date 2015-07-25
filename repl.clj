cf4dd9e3d9194133d54bd598aeced6cbf1c2b865
Martin Bilski <martinb@criticue.com>
2015-07-26 00:02:04 +0200

    ...

--- repl.clj
+++ repl.clj
@@ -1,12 +1,15 @@
 (require 'cljs.repl)
 (require 'cljs.build.api)
-(require 'cljs.repl.browser)
+;(require 'cljs.repl.browser)
+(require 'cljs.repl.node)
 
 (cljs.build.api/build "src"
-                      {:main 'hello-world.core
+                      {:main      'reforms.core
                        :output-to "out/main.js"
-                       :verbose true})
+                       :verbose   true})
 
-(cljs.repl/repl (cljs.repl.browser/repl-env)
+;(cljs.repl/repl (cljs.repl.browser/repl-env)
+(cljs.repl/repl (cljs.repl.node/repl-env)
                 :watch "src"
                 :output-dir "out")
+
