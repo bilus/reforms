81c44d924bf42f8bfdd378dc0cb17a299c08ac27
Martin Bilski <martinb@criticue.com>
2015-07-26 00:01:23 +0200

    Make gen-key-test pass.

--- test/reforms/core/react_keys_test.cljs
+++ test/reforms/core/react_keys_test.cljs
@@ -11,20 +11,25 @@
     (-path [x]
       path)))
 
-(deftest
-  gen-key-test
+(extend-type default
+  IBinding
+  (-valid? [x]
+    false)
+  (-path [x]))
+
+(deftest gen-key-test
   (testing "given no args returns unique id"
-           (is (not= (gen-key) (gen-key))))
+    (is (not= (gen-key) (gen-key))))
   (testing "given nil args returns unique id"
-           (is (not= (gen-key nil nil) (gen-key nil nil))))
+    (is (not= (gen-key nil nil) (gen-key nil nil))))
   (testing "given strings joins them with hyphens"
-           (is (= "label-this-is-something") (gen-key "label" "this-is-something")))
+    (is (= "label-this-is-something") (gen-key "label" "this-is-something")))
   (testing "converts strings into lowercase identifiers"
-           (is (= "long-label-this--is-something") (gen-key "Long label" "This,?is-something")))
+    (is (= "long-label-this--is-something") (gen-key "Long label" "This,?is-something")))
   (testing "cursor, path and symbol"
-           (is (= "path1-path2" (gen-key [:path1 :path2])))
-           (is (= "path1-path2" (gen-key :path1 :path2)))
-           (is (= "foo-cursor-path-bar-path1-path2" (gen-key "foo"
-                                                             (make-cursor [:cursor :path])
-                                                             :bar
-                                                             [:path1 :path2])))))
+    (is (= "path1-path2" (gen-key [:path1 :path2])))
+    (is (= "path1-path2" (gen-key :path1 :path2)))
+    (is (= "foo-cursor-path-bar-path1-path2" (gen-key "foo"
+                                                      (make-cursor [:cursor :path])
+                                                      :bar
+                                                      [:path1 :path2])))))
