d1cae61c84b6117f29101bbf0703ac6836594138
Martin Bilski <martinb@criticue.com>
2015-09-27 19:46:59 +0200

    Upgrade cljs and other libs.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
             :url "http://bilus.github.io/reforms/"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                           [org.clojure/clojurescript "0.0-3308"]]
+            :dependencies [[org.clojure/clojure "1.7.0"]
+                           [org.clojure/clojurescript "1.7.122"]]
             :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
             :auto-clean false
             :source-paths ["src"]
 
-            :plugins [[lein-cljsbuild "1.0.6"]
-                      [lein-doo "0.1.1-SNAPSHOT"]]
+            :plugins [[lein-cljsbuild "1.1.0"]
+                      [lein-doo "0.1.4"]]
 
             :clean-targets ^{:protect false} ["target/"
                                               "resources/public/js/testable.js"]
@@ -22,7 +22,10 @@
                     :defaults                  {:doc/format :markdown}}
 
             :profiles {:doc {:dependencies [[org.clojure/clojurescript "0.0-2985"]]}
-                       :dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]}}
+                       :dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                                            [com.cemerick/piggieback "0.2.1"]
+                                            [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+                             :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
             :aliases {"doc"       ["with-profile" "doc" "doc"]
                       "cljsbuild" ["with-profile" "dev" "cljsbuild"]}
